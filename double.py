b1eecd41e262b1e0d3a558961dc7ca64b192e40a
legenie1 <sergeomoffo1@gmail.com>
2022-06-29 01:49:21 +0100

    All is object

---
+++ double.py
@@ -0,0 +1,3 @@
+def double(a_list):
+    for index, value in enumerate(a_list):
+        a_list[index]=2*value
---
+++ test.py
@@ -0,0 +1,5 @@
+from double import double
+
+things =[2,3,"spam",9.5, [4,7]]
+
+print(things)
